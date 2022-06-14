48a4cda3817c2c4c3ad25a3a6185132de1e6cc4f
Chimoha31 <mokochii1108@gmail.com>
2022-06-14 15:11:57 -0700

    Fixed

--- server/index.js
+++ server/index.js
@@ -41,13 +41,6 @@ io.on("connection", (socket) => {
   })
 })
 
-
-app.use(express.static(path.join(__dirname, "/client/build")));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
-});
-
 server.listen(PORT, () => {
   console.log(`Server is running at Port, ${PORT}`);
 })
