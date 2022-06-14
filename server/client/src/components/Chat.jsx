a3f3bd1d481d1499b93acd28d13975334d8a36e4
Chimoha31 <mokochii1108@gmail.com>
2022-06-14 15:02:59 -0700

    Fixed

---
+++ server/client/src/components/Chat.jsx
@@ -0,0 +1,128 @@
+import React, { useEffect, useState, Fragment } from "react";
+import { useNavigate } from "react-router-dom";
+import ScrollToBottom from "react-scroll-to-bottom";
+import "../styles/Chat.css";
+import IntoRoomMsg from "./IntoRoomMsg";
+import LeftRoomMsg from "./LeftRoomMsg";
+
+const Chat = ({ socket, username, room }) => {
+  const [currentMessage, setCurrentMessage] = useState("");
+  const [messageList, setMessageList] = useState([]);
+  const [intoPerson, setIntoPerson] = useState("");
+  const [isIntoRoom, setIsIntoRoom] = useState(false);
+  const [leftPerson, setLeftPerson] = useState("");
+  const [isLeftRoom, setIsLeftRoom] = useState(false);
+  const navigate = useNavigate();
+
+  const handleSendMessage = async () => {
+    if (currentMessage !== "") {
+      const messageData = {
+        username: username,
+        room: room,
+        message: currentMessage,
+        time:
+          new Date(Date.now()).getHours() +
+          ":" +
+          new Date(Date.now()).getMinutes(),
+      };
+
+      await socket.emit("send_message", messageData);
+      setMessageList((list) => [...list, messageData]);
+      setCurrentMessage("");
+    }
+  };
+
+  const handleLeave = async () => {
+    navigate("/joinroom");
+    const userData = {
+      username: username,
+      room: room,
+    };
+
+    await socket.emit("disconnect_room", userData);
+    console.log(userData);
+  };
+
+  useEffect(() => {
+    socket.on("into_room", (data) => {
+      setIsIntoRoom(true);
+      setIntoPerson(data.username);
+    });
+
+    return () => socket.off("into_room");
+    // eslint-disable-next-line
+  }, []);
+
+  useEffect(() => {
+    socket.on("left_chat", (data) => {
+      setIsLeftRoom(true);
+      console.log(data.username);
+      setLeftPerson(data.username);
+    });
+    return () => socket.off("left_chat");
+    // eslint-disable-next-line
+  }, []);
+
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      console.log(data);
+      setMessageList((list) => [...list, data]);
+    });
+    return () => socket.off("receive_message");
+    // eslint-disable-next-line
+  }, []);
+
+  return (
+    <Fragment>
+      <div className="chat-window">
+        <div className="chat-header">
+          <p>Live Chat</p>
+          <div>
+            <p className="leave" onClick={handleLeave}>
+              Leave 👋🏻
+            </p>
+          </div>
+        </div>
+
+        <div className="chat-body">
+          <ScrollToBottom className="message-container">
+            {isIntoRoom && <IntoRoomMsg intoPerson={intoPerson} />}
+
+            {messageList.map((message, index) => (
+              <div
+                key={index}
+                className="message"
+                id={username === message.username ? "you" : "other"}
+              >
+                <div>
+                  <div className="message-content">
+                    <p>{message.message}</p>
+                  </div>
+                  <div className="message-meta">
+                    <p>{message.time}</p>
+                    <p id="author">{message.username}</p>
+                  </div>
+                </div>
+              </div>
+            ))}
+            {isLeftRoom && <LeftRoomMsg leftPerson={leftPerson} />}
+          </ScrollToBottom>
+        <div className="chat-footer">
+          <input
+            type="text"
+            placeholder="message"
+            value={currentMessage}
+            onChange={(e) => setCurrentMessage(e.target.value)}
+            onKeyPress={(e) => {
+              e.key === "Enter" && handleSendMessage();
+            }}
+          />
+          <button onClick={handleSendMessage}>&#9658;</button>
+        </div>
+      </div>
+        </div>
+    </Fragment>
+  );
+};
+
+export default Chat;
---
+++ server/client/src/components/JoinForm.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import Header from './Header';
+import "../styles/JoinForm.css";
+import { Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+
+const JoinForm = ({ socket, username, setUsername, room ,setRoom }) => {
+  const [errMsg, setErrMsg] = useState("");
+  const navigate = useNavigate();
+
+  const handleJoinRoom = () => {
+    if (username !== "" && room !== "") {
+      socket.emit("join_room", {username, room});
+      navigate("/chatroom");
+    }else if(username === "" || room === "") {
+      setErrMsg("Fill in the blank is required")
+    }
+  };
+
+  return (
+    <div className="container_bg">
+      <Header />
+      <div className="joinform_container">
+        <div className="form">
+          <h3> Join A Chat !!</h3>
+          <label>*User Name</label>
+          <input
+            type="text"
+            placeholder="Your name"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          {errMsg && !username ? <p>{errMsg}</p> : ""}
+          <label>*Room Number</label>
+          <input
+            type="text"
+            placeholder="Room number"
+            value={room}
+            onChange={(e) => setRoom(e.target.value)}
+          />
+          {errMsg && !room ? <p>{errMsg}</p> : ""}
+          <Button onClick={handleJoinRoom} className="roomform_btn">
+            Join A Room
+          </Button>
+        </div>
+
+        <div className="form_gif">{/* {bg} */}</div>
+      </div>
+    </div>
+  );
+};
+
+export default JoinForm;
